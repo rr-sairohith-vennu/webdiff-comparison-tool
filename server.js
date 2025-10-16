b2204bae1224c4e7e2737501edad56917954db88
Sairohith Vennu <sairohith.vennu@rakuten.com>
2025-10-15 20:39:38 -0700

    Add request logging to debug screenshot 406 errors

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ const RESULTS_DIR = join(DATA_DIR, 'results');
 
 // Middleware
 app.use(express.json());
+
+// Add logging for all requests
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.static('public'));
 app.use('/screenshots', express.static(SCREENSHOTS_DIR, {
   setHeaders: (res, path) => {
